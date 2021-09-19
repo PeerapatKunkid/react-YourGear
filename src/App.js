dff9eb956c348c8a7a38c69352f64f4701ded02f
lastteleeflags <lastteleeflags@gmail.com>
2021-09-19 16:14:12 +0700

    LinkAPI & CategoryPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+import React, { useState, useEffect, Component } from "react";
+import "./App.css";
+import AppBanner from "./components/AppBanner";
+import AppCategory from "./components/AppCategory";
+import ContentRecommend from "./components/ContentRecommend";
+import KeyboardCategory from "./components/KeyboardCategory";
 
-import './App.css';
-import AppBanner from './components/AppBanner';
-import AppCategory from './components/AppCategory';
-import ContentRecommend from './components/ContentRecommend';
-import KeyboardCategory from './components/KeyboardCategory';
-import React, { Component } from "react";
 import Slider from "react-slick";
 
-
 function App() {
+  
+
   return (
     <div className="App">
-      <AppBanner/>
+      <AppBanner />
       {/* <AppCategory/>
       <ContentRecommend/>  */}
-      <KeyboardCategory/>
-
-
+      <KeyboardCategory />
     </div>
   );
 }
 
 export default App;
-
--- src/components/KeyboardCategory.js
+++ src/components/KeyboardCategory.js
@@ -1,24 +1,33 @@
 import "./KeyboardCategory.css";
 import $ from "jquery";
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect, Component } from "react";
 
 function KeyboardCategory() {
+  const [keyboard, setKeyboard] = useState([]);
+  useEffect(() => {
+    getAllKeyboard();
+    
+  }, []);
+
+  console.log(keyboard);
+  async function getAllKeyboard() {
+    const res = await axios.get("http://localhost:3001/allKeyboard/1");
+    const data = res.data;
+    setKeyboard(data);
+  }
+
   return (
-    
     <div class="main-content-category">
-      
       <header class="page-header">
-    
-      <div class="dropdown">
-  <button class="dropbtn">TYPE</button>
-  <div class="dropdown-content">
-  <option value="low-high">Low - High</option>
-  <option value="high-low">High - LOW</option>
-  
-  </div>
-  </div>
-    
-  </header>
+        <div class="dropdown">
+          <button class="dropbtn">TYPE</button>
+          <div class="dropdown-content">
+            <option value="low-high">Low - High</option>
+            <option value="high-low">High - LOW</option>
+          </div>
+        </div>
+      </header>
       <div class="grid">
         {/* ส่วนของ FILTER ด้านซ้าย */}
         <aside class="page-leftbar">
@@ -69,46 +78,109 @@ function KeyboardCategory() {
             </label>
             <p class="price-tag-filter">PRICE</p>
             <tr border="1" class="filter-price">
-              <td><input type="text" id="fname" name="fname"/></td>
+              <td>
+                <input type="text" id="fname" name="fname" />
+              </td>
               <td class="to"> ~ </td>
-              <td><input type="text" id="fname" name="fname"/></td>
+              <td>
+                <input type="text" id="fname" name="fname" />
+              </td>
             </tr>
             <p class="filter-header-category">CATEGORY</p>
             <div class="linkcategory">
-            <p>
-                - Mouse
-            </p>
-            <p>
-                - Keyboard
-            </p>
-            <p>
-                - Headset
-            </p>
-            <p>
-                - Mousepad
-            </p>
-            <p>
-                - Microphone
-            </p>
+              <p>- Mouse</p>
+              <p>- Keyboard</p>
+              <p>- Headset</p>
+              <p>- Mousepad</p>
+              <p>- Microphone</p>
             </div>
           </div>
         </aside>
         {/* ส่วนของ Maincontent */}
         <main class="page-main">
           <div class="content-data-category">
-          
+            <div class="grid-category-filter">
+              {keyboard.map((item) => {
+                return (
+                  <div class="item">
+                    <div class="card-content">
+                      <img
+                        class="card-img"
+                        src={
+                          item.advice[0]
+                            ? item.advice[0].data[0].image
+                            : item.banana[0]
+                            ? item.banana[0].data[0].image
+                            : item.mercular[0]
+                            ? item.mercular[0].data[0].image
+                            : ""
+                        }
+                        height="250"
+                        width="10"
+                      ></img>
+                      <p class="category-box">{item.name}</p>
+                      <div class="container">
+                        <table class="card-box">
+                          <tr>
+                            <td>
+                              <img
+                                class="img-recommend"
+                                src="https://notebookspec.com/laravel/public//images/component-shop-advice.jpg"
+                                width="110"
+                                height="40"
+                              />
+                            </td>
+                            <td>
+                              {item.advice[0]
+                                ? item.advice[0].data[0].price
+                                : "N/A"}
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <img
+                                class="img-recommend"
+                                src="https://notebookspec.com/laravel/public//images/component-shop-jib.jpg"
+                                width="110"
+                                height="40"
+                              />
+                            </td>
+                            <td>
+                              {item.mercular[0]
+                                ? item.mercular[0].data[0].price
+                                : "N/A"}
+                            </td>
+                          </tr>
 
-
-
+                          <tr>
+                            <td>
+                              <img
+                                class="img-recommend"
+                                src="https://notebookspec.com/laravel/public//images/component-shop-banana.png"
+                                width="110"
+                                height="40"
+                              />
+                            </td>
+                            
+                              <td>
+                                {item.banana[0]
+                                  ? item.banana[0].data[0].price
+                                  : "N/A"}
+                              
+                            </td>
+                          </tr>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </main>
       </div>
     </div>
   );
 }
-
-
-
-
 
 export default KeyboardCategory;
